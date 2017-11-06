040b927faa609fa25d82f9ebd483432026687a21
JuliaPokitko <pokitkojulia@gmail.com>
2017-11-06 13:29:31 +0200

    Task one

---
+++ Task0.java
@@ -0,0 +1,9 @@
+public class Task0 {
+    public static void main(String[] args) {
+        int low = 8;
+        int high = 10;
+        if (low < high) {
+            System.out.println("Low is smaller then high");
+        }
+    }
+}
---
+++ Task01.java
@@ -0,0 +1,11 @@
+public class Task01 {
+    public static void main(String[] args) {
+        int[] newArray = new int[10];
+        for (int i = 0; i<10; i++){
+            newArray[1] = i;
+        }
+        for (int i = 0; i < 10; i++){
+            System.out.println(newArray[i]);
+        }
+    }
+}
